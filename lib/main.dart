76e89ffeb52f8292f97ffbcb2bae82b75f36f153
Cassiana Soares <cassianassilva@hotmail.com>
2020-06-24 23:51:09 -0300

    start commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/screens/todo_list_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Todo List',
+      debugShowCheckedModeBanner:  false,
+      theme: ThemeData(
+      
+        primarySwatch: Colors.red,
+        
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: TodoListScreen(),
+    );
+  }
+}
+
